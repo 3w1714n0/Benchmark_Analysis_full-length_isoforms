b3286a1fe4f9d495bcb250a5d30a5fb6df1146d7
Emiliano Navarro <82102364+3w1714n0@users.noreply.github.com>
2022-04-26 10:57:31 +0200

    Update Length_Distribution.R

--- RScripts/Length_Distribution.R
+++ RScripts/Length_Distribution.R
@@ -7,7 +7,7 @@ rm(list= ls())
 # Set working directory
 setwd("C:/Users/enava/OneDrive - Universitat Autònoma de Barcelona/CUARTO/TFG/Análisis/sequins")
 
-#1. Read data
+# 1. Read raw data
 reads_Ilum<-read.csv(file="ilum_length_distribution.txt", sep="", header=FALSE)
 reads_Nano<-read.csv(file="nano_length_distribution.txt", sep="", header=FALSE)
 
@@ -20,28 +20,10 @@ reads_Ilum$V2
 #Nanopore total reads
 sum(reads_Nano$V2)
 
-# Adding sequentiation method
-reads_Ilum$Type<-"Illumina"
-reads_Nano$Type<-"Nanopore"
-
-#Combinig the datasets
-reads<-rbind(reads_Ilum, reads_Nano)
-
-# 2. Plotting the length distribution
-library(ggplot2)
-
-ggplot(reads, mapping = aes(x = reorder(V1, V1), y = V2, fill = Type))+
-  geom_bar(stat = "identity")+
-  theme_bw()+
-  scale_fill_manual(values = c("#fcc100","#2cc0e5"))+
-  labs(x = "Read length", y="Number of reads")+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  facet_grid(row = vars(Type), scales = "free")
-
-# 1. Read data
+# 2. Read the merged data
 ld <- read.csv(file="length_distribution.txt", sep="", header=FALSE)
 
-# 2. Plotting the length distribution
+# 3. Plotting the length distribution
 library(ggplot2)
 
 ggplot(ld, mapping = aes(x = order(as.integer(V2)), y = V3, fill = V4))+
