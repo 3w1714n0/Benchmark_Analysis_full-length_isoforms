6c4a66c67b79176a5ee33a8a7da95066c2713cc8
Emiliano Navarro <82102364+3w1714n0@users.noreply.github.com>
2022-05-20 20:27:14 +0200

    Add files via upload

---
+++ RScripts/Journals figure.R
@@ -0,0 +1,20 @@
+### Scimago Ranking Plot
+### Emiliano Navarro, 2022
+
+# Clean workspace
+rm(list= ls())
+
+# Set working directory
+setwd("C:/Users/enava/Desktop")
+
+# Read the data
+journals <- c("Genome Biology", "Nucleic Acids Research", "Genome Research", "Genomics, Proteomics and Bioinformatics", "BMC Bioinformatics")
+sjr <- c(9.371, 8.241, 5.707, 2.246, 1.246)
+info <- data.frame(journals, sjr)
+
+library(ggplot2)
+
+ggplot(data = info, mapping = aes(x = reorder(journals, -sjr), y = sjr), fill = journals)+
+  geom_col(fill = "#5A774E")+
+  theme_bw()+
+  labs(x = "Journals", y = "SJR (SCImago Journal Rank indicator)")
