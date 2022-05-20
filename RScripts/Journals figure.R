8502d931c6bab74748322a8c7ba6892e6d15e4bb
Emiliano Navarro <82102364+3w1714n0@users.noreply.github.com>
2022-05-20 20:27:51 +0200

    Update Journals figure.R

--- RScripts/Journals figure.R
+++ RScripts/Journals figure.R
@@ -12,8 +12,10 @@ journals <- c("Genome Biology", "Nucleic Acids Research", "Genome Research", "Ge
 sjr <- c(9.371, 8.241, 5.707, 2.246, 1.246)
 info <- data.frame(journals, sjr)
 
+# Load the required library
 library(ggplot2)
 
+# Plot
 ggplot(data = info, mapping = aes(x = reorder(journals, -sjr), y = sjr), fill = journals)+
   geom_col(fill = "#5A774E")+
   theme_bw()+
